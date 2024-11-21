f05cdb84f7183cc8e8f5b7d1ff4ed2bb9c40cff0
Adamhanafi22 <adamhanafi1922@gmail.com>
2024-11-21 21:48:03 +0700

    membuat set mkPilihan dan kelasPilihan

--- app/src/main/java/com/example/praktikum6/ui/view/viewmodel/RencanaStudyViewModel.kt
+++ app/src/main/java/com/example/praktikum6/ui/view/viewmodel/RencanaStudyViewModel.kt
@@ -8,6 +8,15 @@ class RencanaStudyViewModel : ViewModel() {
     private val _krsState = MutableStateFlow(RencanaStudi())
     val krsStateUi: StateFlow<RencanaStudi> = _krsState.asStateFlow()
 
+    fun setMataKuliah(mkPilihan: String){
+        _krsState.update { stateMK -> stateMK.copy(namaMK = mkPilihan)}
+    }
+    fun setKelas(kelasPilihan: String){
+        _krsState.update {stateKelas -> stateKelas.copy(kelas = kelasPilihan)}
+
+
+    }
+
 
 
 
