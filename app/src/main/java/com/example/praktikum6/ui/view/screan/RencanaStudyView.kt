3272437d7db1baec41180e0c18280d54126115c7
Adamhanafi22 <adamhanafi1922@gmail.com>
2024-11-21 22:02:47 +0700

    membuat variabel dan column

--- app/src/main/java/com/example/praktikum6/ui/view/screan/RencanaStudyView.kt
+++ app/src/main/java/com/example/praktikum6/ui/view/screan/RencanaStudyView.kt
@@ -1,6 +1,13 @@
 package com.example.praktikum6.ui.view.screan
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
 import com.example.praktikum6.model.Mahasiswa
 import com.example.praktikum6.ui.view.viewmodel.RencanaStudyViewModel
 
@@ -10,5 +17,21 @@ fun RencanaStudyView(
     onSubmitButtonClicked: (MutableList<String>)-> Unit,
     onBackButtonClicked: ()-> Unit
 ){
-    var
+    var chosenDropdown by remember {
+        mutableStateOf(
+            ""
+        )
+    }
+    var checked by remember { mutableStateOf(false) }
+    var pilihanKelas by remember {
+        mutableStateOf("")
+    }
+    var listData: MutableList<String> = mutableListOf(chosenDropdown, pilihanKelas)
+    Column (
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = colorResource(is = R.color.primary))
+
+    )
+
 }
