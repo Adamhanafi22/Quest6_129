5b410bd79ffd141d287422857fa0210a8c4f8090
Adamhanafi22 <adamhanafi1922@gmail.com>
2024-11-21 21:55:52 +0700

    membuat funRencanastudyview

---
+++ app/src/main/java/com/example/praktikum6/ui/view/screan/RencanaStudyView.kt
@@ -0,0 +1,14 @@
+package com.example.praktikum6.ui.view.screan
+
+import androidx.compose.runtime.Composable
+import com.example.praktikum6.model.Mahasiswa
+import com.example.praktikum6.ui.view.viewmodel.RencanaStudyViewModel
+
+@Composable
+fun RencanaStudyView(
+    mahasiswa: Mahasiswa,
+    onSubmitButtonClicked: (MutableList<String>)-> Unit,
+    onBackButtonClicked: ()-> Unit
+){
+    var
+}
