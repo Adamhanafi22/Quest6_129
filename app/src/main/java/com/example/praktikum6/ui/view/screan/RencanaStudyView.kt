54174dd25fe09773f8f0fef9611ce96a8ba69585
Adamhanafi22 <adamhanafi1922@gmail.com>
2024-11-21 22:11:49 +0700

    membuat Text mahasiswa nim

--- app/src/main/java/com/example/praktikum6/ui/view/screan/RencanaStudyView.kt
+++ app/src/main/java/com/example/praktikum6/ui/view/screan/RencanaStudyView.kt
@@ -3,11 +3,13 @@ package com.example.praktikum6.ui.view.screan
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -16,7 +18,9 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.praktikum6.model.Mahasiswa
 import com.example.praktikum6.ui.view.viewmodel.RencanaStudyViewModel
 
@@ -55,6 +59,21 @@ fun RencanaStudyView(
                     .size(50.dp)
 
             )
+            Spacer(modifier = Modifier.padding(start = 16.dp))
+            Column (modifier = Modifier.weight(if)){
+                Text(
+                    text = mahasiswa.nama,
+                    fontWeight = FontWeight.Light,
+                    fontSize = 12.sp,
+                    color = color.white
+                )
+                Text(
+                    text = mahasiswa.nim,
+                    fontWeight = FontWeight.Light,
+                    fontSize = 12.sp,
+                    color = color.white
+                )
+            }
         }
     }
 
