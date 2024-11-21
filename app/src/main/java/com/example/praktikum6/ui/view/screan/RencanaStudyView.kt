24396d36ac7a018345e1c7b7aada6d3c953ab941
Adamhanafi22 <adamhanafi1922@gmail.com>
2024-11-21 22:23:04 +0700

    Box background

--- app/src/main/java/com/example/praktikum6/ui/view/screan/RencanaStudyView.kt
+++ app/src/main/java/com/example/praktikum6/ui/view/screan/RencanaStudyView.kt
@@ -1,6 +1,7 @@
 package com.example.praktikum6.ui.view.screan
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -9,18 +10,24 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.praktikum6.R
 import com.example.praktikum6.model.Mahasiswa
 import com.example.praktikum6.ui.view.viewmodel.RencanaStudyViewModel
 
@@ -60,7 +67,8 @@ fun RencanaStudyView(
 
             )
             Spacer(modifier = Modifier.padding(start = 16.dp))
-            Column (modifier = Modifier.weight(if)){
+            Column (modifier = Modifier.weight(if)
+            ){
                 Text(
                     text = mahasiswa.nama,
                     fontWeight = FontWeight.Light,
@@ -71,10 +79,28 @@ fun RencanaStudyView(
                     text = mahasiswa.nim,
                     fontWeight = FontWeight.Light,
                     fontSize = 12.sp,
-                    color = color.white
+                    color = Color.White
                 )
             }
+                Box {
+                    Icon(
+                        imageVector = Icons.filled.Notification,
+                        contentDescription = "",
+                        Lint = Color.White
+                    )
+            }
         }
+        Box(modifier = Modifier
+            .background(
+                color = Color.White,
+                shape = RoundedCornerShape(
+                    topEnd = 15.dp,
+                    topStart = 15.dp
+                )
+            )
+            .fillMaxSize(),
+
+        )
     }
 
 }
