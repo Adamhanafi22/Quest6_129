3b0345002d0fe5e1bd2cb52c0b66d352a8f41815
Adamhanafi22 <adamhanafi1922@gmail.com>
2024-11-21 23:17:44 +0700

    membuat ExposedDropdownMenuBox

--- app/src/main/java/com/example/praktikum6/ui/view/widget/DynamicSelectedField.kt
+++ app/src/main/java/com/example/praktikum6/ui/view/widget/DynamicSelectedField.kt
@@ -2,7 +2,13 @@ package com.example.praktikum6.ui.view.widget
 
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuBoxScope
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -15,5 +21,16 @@ fun DynamicSelectedTextField(
     onValueChangeEvent: (String) -> Unit,
     modifier: Modifier = Modifier
 ){
-    var
+    var expanded by remember { mutableStateOf(false) }
+
+    ExposedDropdownMenuBox(
+        expanded = expanded,
+        onExpandedChange = {expanded = !expanded},
+        modifier = Modifier
+
+    ){
+
+    }
+
+
 }
